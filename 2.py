716d5fa738b1415646a03b535618c71859b4218e
r.dorokhova <r.d>
2022-12-19 13:35:46 +0300

    iteration of values fixed

--- 2.py
+++ 2.py
@@ -5,11 +5,7 @@ def check_statement(x,y,z):
     print("¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z :", (not (x or y or z) == (not x and not y and not z)),
      " for X =", x, ", Y = ", y, ", Z = ", z)
 
-check_statement(1, 0, 0)
-check_statement(1, 1, 1)
-check_statement(1, 0, 1)
-check_statement(1, 1, 0)
-check_statement(0, 0, 0)
-check_statement(0, 1, 1)
-check_statement(0, 0, 1)
-check_statement(0, 1, 0)
+for X in [True, False]:
+        for Y in [True, False]:
+            for Z in [True, False]:
+                check_statement(X, Y, Z)
